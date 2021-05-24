5b4af69af15bc3d1c5c9e155a540568fe4595d85
Nick <ngrisafi@rippling.com>
2021-05-24 16:55:57 -0700

    Cleaned up wildcard context manager nesting

--- pants-plugins/wildcard_imports/wildcard_imports.py
+++ pants-plugins/wildcard_imports/wildcard_imports.py
@@ -1,14 +1,11 @@
 from pants.engine.goal import Goal, GoalSubsystem, LineOriented
 from pants.engine.rules import collect_rules, goal_rule
 from pants.engine.console import Console
-from pants.engine.target import Sources, Target, Targets
-from pants.engine.rules import Get, rule
-from pants.engine.target import HydratedSources, HydrateSourcesRequest
+from pants.engine.target import Sources, Targets
+from pants.engine.rules import Get
 from pants.core.util_rules.source_files import SourceFiles, SourceFilesRequest
-from pants.engine.fs import SpecsSnapshot
 
 from pants.engine.fs import Digest, DigestContents
-# from pants.engine.target import SourcesPathsRequest, SourcesPaths
 
 
 from pants.backend.python.target_types import (
@@ -29,11 +26,19 @@ class WildcardImports(Goal):
 async def wildcard_imports(
     console: Console, wildcard_imports_subsystem: WildcardImportsSubsystem, targets: Targets
 ) -> WildcardImports:
+    sources = await Get(
+        SourceFiles,
+        SourceFilesRequest(
+            [tgt.get(Sources) for tgt in targets], for_sources_types=(PythonSources,), enable_codegen=False
+        ),
+    )
+    digest_contents = await Get(DigestContents, Digest, sources.snapshot.digest)
     with wildcard_imports_subsystem.line_oriented(console) as print_stdout:
-        sources = await Get(SourceFiles, SourceFilesRequest([tgt.get(Sources) for tgt in targets], for_sources_types=(PythonSources,)))
-        digest_contents = await Get(DigestContents, Digest, sources.snapshot.digest)
         print_stdout(digest_contents)
     return WildcardImports(exit_code=1)
+
+def has_wildcard_import() -> bool:
+    ...
 
 
 def rules():
