38f358c3b09499fc983731e66ac3ae71418a0876
Nick <ngrisafi@rippling.com>
2021-05-24 16:44:13 -0700

    Updated to use list comp for fetching sources (performance improvement)

--- pants-plugins/wildcard_imports/wildcard_imports.py
+++ pants-plugins/wildcard_imports/wildcard_imports.py
@@ -25,35 +25,13 @@ class WildcardImports(Goal):
     subsystem_cls = WildcardImportsSubsystem
 
 
-# @goal_rule
-# async def wildcard_imports(
-#     console: Console, wildcard_imports_subsystem: WildcardImportsSubsystem, targets: Targets
-# ) -> WildcardImports:
-#     sources_call = []
-#     with wildcard_imports_subsystem.line_oriented(console) as print_stdout:
-#         for target in targets:
-#             sources. await Get(
-#                 HydratedSources,
-#                 HydrateSourcesRequest(target.get(Sources), for_sources_types=(PythonSources,), enable_codegen=True),
-#             )
-#             # sources = Get(SourcesPaths, SourcesPathsRequest(target.get(Sources), for_sources_types=(PythonSources,)))
-#             print_stdout(source_files)
-#         sources = await Get(
-#             SourceFiles,
-#             SourceFilesRequest([targets]) 
-#         )
-#         print_stdout(sources)
-#     return WildcardImports(exit_code=1)
-
 @goal_rule
 async def wildcard_imports(
-    console: Console, wildcard_imports_subsystem: WildcardImportsSubsystem, specs_snapshot: SpecsSnapshot
+    console: Console, wildcard_imports_subsystem: WildcardImportsSubsystem, targets: Targets
 ) -> WildcardImports:
     with wildcard_imports_subsystem.line_oriented(console) as print_stdout:
-        print_stdout(specs_snapshot.snapshot.digest)
-        for f in specs_snapshot.snapshot.files:
-            # sources = Get(SourcesPaths, SourcesPathsRequest(target.get(Sources), for_sources_types=(PythonSources,)))
-            print_stdout(f)
+        sources = await Get(SourceFiles, SourceFilesRequest([tgt.get(Sources) for tgt in targets]))
+        print_stdout(sources.snapshot.digest)
     return WildcardImports(exit_code=1)
 
 
