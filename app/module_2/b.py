2e0cafa4f54557c56306c8654f541d4c74c373a2
Nick Grisafi <njgrisafi@gmail.com>
2021-06-06 06:41:14 -0700

    Updating example

--- app/module_2/b.py
+++ app/module_2/b.py
@@ -1,8 +1,9 @@
-from module_common.utils import *
-
 from module_2.a import example_a
+from module_common.utils import *
+from module_common.utils import printer_2
 
 
 def example_b() -> None:
     printer("example")
+    printer_2("something")
     example_a()
