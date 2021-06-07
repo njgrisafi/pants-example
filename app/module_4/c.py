b918229448baebdb1fd2eac238422e961a05529d
Nick <ngrisafi@rippling.com>
2021-06-07 16:44:07 -0700

    pdated example for nest imports

--- app/module_4/c.py
+++ app/module_4/c.py
@@ -1,3 +1,10 @@
 from module_common import *
+from module_common.utils import printer
 
 log = logging.getLogger(__name__)
+printer("test")
+
+
+def some_func():
+    from module_common.utils import printer
+    printer("testing")
